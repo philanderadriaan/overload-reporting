bc42bc895334563fb3d12b7843df555c0b7c6578
philanderadriaan <philander90@msn.com>
2014-08-06 10:43:25 -0700

    Moved project from network drive to local drive.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace nksd_overload_reporting
         static void Main(string[] args)
         {
             Console.WriteLine("test");
+            IEnumerable<string> e = File.ReadLines("sql-2000-query-1");
         }
     }
 }
