400e83da3ab40e947b8ffd8b883fc64b9858d6c2
philanderadriaan <philander90@msn.com>
2014-08-08 10:07:07 -0700

    Added method to generate full paths.

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace nksd_overload_reporting
 {
-    class StringFormatter
+    class Formatter
     {
+        private string my_folder;
+
+        public Formatter()
+        {
+
+        }
+
+        public string generateReportPath(string name)
+        {
+            return my_folder + name + ".csv";
+        }
     }
+
+
 }
