941b17329845e739c4a55a145d8f964f15e2b53d
philanderadriaan <philander90@msn.com>
2014-08-14 14:40:10 -0700

    Query update.

--- bin/Debug/sql-query/test.sql
+++ bin/Debug/sql-query/test.sql
@@ -1,9 +1,10 @@
 select *
-from classsizes.dbo.classoverloadrules
-where penaltytype is not null
-and penaltygroup is not null
-and lowstudents is not null
-and highstudents is not null
-and amount is not null
-and usage is not null
-and period = 8
+from classsizes.dbo.classes
+where schoolyear = 2011
+--where penaltytype is not null
+--and penaltygroup is not null
+--and lowstudents is not null
+--and highstudents is not null
+--and amount is not null
+--and usage is not null
+--and period = 8
