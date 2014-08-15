a3b911308b4e8604dcc5077887c6f867eb17099e
philanderadriaan <philander90@msn.com>
2014-08-15 09:18:58 -0700

    Fixed overwriting bug.

--- bin/Debug/sql-query/test.sql
+++ bin/Debug/sql-query/test.sql
@@ -1,10 +1,11 @@
 select *
-from classsizes.dbo.classes
-where schoolyear <> 2011
+from classsizes.dbo.classoverloadrules
+--where schoolyear <> 2011
 --where penaltytype is not null
 --and penaltygroup is not null
 --and lowstudents is not null
 --and highstudents is not null
 --and amount is not null
 --and usage is not null
---and period = 8
+where period = 8
+and coursesubject = 11
