19e1cdaf4a01758a5be2d7ce1295c6f104c77859
philanderadriaan <philander90@msn.com>
2014-08-18 11:53:00 -0700

    Added ODBC connector.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace nksd_overload_reporting
     {
         static void Main(string[] args)
         {
+            /*
             DataManager manager = new DataManager();
             manager.read("test.sql");
 
@@ -25,6 +27,40 @@ namespace nksd_overload_reporting
                 string name = f.generateFileName(i);
                 manager.write(name);
             }
+             * */
+
+            Retrieve();
+        }
+
+
+        private static void Retrieve()
+        {
+            string con = //"DRIVER={MySQL ODBC 3.51 Driver};" +
+            "SERVER=skywarddata;" +
+            "DATABASE=nkitsas;" +
+            "UID=roODBCUser01;" +
+            "PASSWORD=Pupl3P1aneD0wn;" +
+            "OPTION=0";
+
+            OdbcConnection Conn = new System.Data.Odbc.OdbcConnection(con);
+
+            //OdbcCommand catCMD = new OdbcCommand("SELECT * FROM [Enter table name here]", Conn);
+
+            //Conn.Open();
+
+            //OdbcDataReader myReader = catCMD.ExecuteReader();
+
+            //while (myReader.Read())
+            {
+                //the SqlDataReader returns typed data.  You must use the
+                //proper get method for the type you are retrieving.
+                //(GetInt32, GetString, GetBoolean etc.)
+                //Response.Write(myReader.GetString(myReader.GetOrdinal("[Enter column name here]")));
+                //Response.Write("<BR>");
+            }
+
+            //myReader.Close();
+            //Conn.Close();
         }
     }
 }
